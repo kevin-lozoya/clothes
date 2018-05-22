51236bb39689e38b5e193a59a253efc6b5a2eac3
Kevin Lozoya <kevinlozoyaginer@gmail.com>
2018-05-22 23:33:42 +0200

    Crear Índice

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,12 +1,32 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Brand;
+use App\Models\Color;
+use App\Models\Style;
+use App\Models\Product;
+use App\Models\Category;
 use App\Modules\Template;
+use App\Models\Subcategory;
 
 class MainController extends Template {
   
   public function getIndex() {
-    return $this->render('main/index.twig');
+    $products = Product::orderBy('id', 'desc')->get();
+    $brands = Brand::orderBy('description', 'asc')->get();
+    $categories = Category::orderBy('description', 'asc')->get();
+    $subcategories = Subcategory::orderBy('description', 'asc')->get();
+    $styles = Style::orderBy('description', 'asc')->get();
+    $colors = Color::orderBy('description', 'asc')->get();
+
+    return $this->render('main/index.twig', [
+      'products' => json_encode($products),
+      'brands' => $brands,
+      'categories' => $categories,
+      'subcategories' => json_encode($subcategories),
+      'styles' => $styles,
+      'colors' => $colors,
+    ]);
   }
 }
 ?>
